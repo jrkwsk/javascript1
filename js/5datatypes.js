7448a12c5eac795fab7a5ae3b31c1f517a2c585e
jrkwsk <ula.jurowska@gmail.com>
2017-12-17 12:20:31 +0100

    datatypes skonczone

---
+++ js/5datatypes.js
@@ -0,0 +1,31 @@
+'use strict'; 
+
+var czyJestSmog;
+czyJestSmog = false;
+
+if (czyJestSmog) {
+    console.log ("Jest SMOG");
+} else {
+    console.log ("nie ma SMOGU");
+}
+
+
+
+
+
+/*var wyplataStr = "5000";
+var premiaStr = "2500";
+var calkowitaWyplataStr; 
+
+calkowitaWyplataStr = wyplataStr + premiaStr;
+
+console.log(calkowitaWyplataStr);*/
+
+
+//var wyplata = 5000;
+//var premia = 2500;
+//var calkowitaWyplata; 
+//
+//calkowitaWyplata = wyplata + premia;
+//
+//console.log(calkowitaWyplata);
