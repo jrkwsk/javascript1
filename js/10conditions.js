1662c825073bc7f6ba6fcb75f17942f32c0ba94f
jrkwsk <ula.jurowska@gmail.com>
2017-12-17 15:54:13 +0100

    loops

---
+++ js/10conditions.js
@@ -0,0 +1,25 @@
+'use strict';
+var wzrostMateusza = 190;
+var wzrostOlgi = 152;
+
+if (wzrostMateusza > wzrostOlgi) {
+    console.log("Olga jest niższa :)");
+}
+
+//switch 
+
+var color = 'czarny';
+
+switch (color) {
+    case 'czerwony':
+        console.log('kolor czerwony')
+        break;
+    case 'zielony':
+        console.log('kolor zielony')
+        break;
+    case 'niebieski':
+        console.log('kolor niebieski')
+        break;
+    default:
+        console.log('inny kolor')
+}
---
+++ js/11loops.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// pętla for
+
+//for (var i = 10; i > 0; i--) {
+//    console.log(i);
+//}
+
+//petla for each
+
+
+//
+//tablica.forEach (function(element, index) {
+//    console.log ("Element z Indexem: "+ index + " ma wartość: " + element );
+//}); 
+//
+
+//nie dziala 
+
+var tablica = ["Krystian", "Monika", "Danuta"]; 
+for (var tablica = 0; tablica < tablica.lenght; tablica ++) {
+    console.log("Elament z Indexem: " + i + " ma wartość: " + tablica);
+}
