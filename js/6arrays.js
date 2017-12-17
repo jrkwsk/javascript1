a94cfe5a011f7a67272346a265772e0f4e472478
jrkwsk <ula.jurowska@gmail.com>
2017-12-17 12:50:55 +0100

    skonczone arrays

---
+++ js/6arrays.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var imiona = ['Monika', 'Kystian', 'Łukasz'];
+
+imiona [3] = 'Tania';
+
+imiona [2] = 'Ela';
+
+imiona.push ('Geralt');
+
+console.log (imiona);
+
+console.log (imiona[2] + " i " + imiona [0]);
+
+imiona.pop ();
+imiona.pop ();
+
+/*console.log (imiona);
+
+console.log (imiona.unshift('Robert'));
+
+console.log (imiona);
+
+console.log (imiona.shift());
+
+console.log (imiona);
+
+console.log (imiona.length);
+
+imiona.push ('Kaja'); 
+
+console.log (imiona);*/
+
+/*var stringzTablicy = imiona.join(" - ");
+
+console.log(stringzTablicy); */
+
+console.log(imiona);
+
+console.log(imiona.reverse());
+
+var tabelaLiczb = [1,6,5,9,55,33,69,7,8,21];
+
+var posortowanaTablica = tabelaLiczb.sort();
+
+console.log (posortowanaTablica);
