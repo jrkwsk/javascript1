067291ddf5e4bf9bd78749cf0fd9bbca0b6c83b4
jrkwsk <ula.jurowska@gmail.com>
2017-12-17 15:01:31 +0100

    opreacje skoczone

---
+++ js/8objectmath.js
@@ -0,0 +1,5 @@
+/*console to wbudowany w przeglądarkę obiekt*/
+/*obiekt to też math*/
+
+console.log (Math.PI);
+console.log (Math.cos(0));
---
+++ js/9operatos.js
@@ -0,0 +1,22 @@
+//modulo
+
+var liczba1 =13, liczba2= 5; 
+ var wynik;
+wynik = liczba1 % liczba2;
+console.log (wynik)
+
+liczba1 = liczba1 + liczba2;
+console.log(liczba1);
+
+console.log(liczba1);
+
+
+//można zapisywać też tak operacje 
+
+liczba1 -= liczba2;
+ 
+console.log(liczba1);
+
+//inne logiczne struktury
+
+(8<4) ? console.log ("prawda") : console.error ("Fałsz");
